119b39f1c285cbb09a49941d4483831dd808dd5b
Илья <i.ivannikov@molga.ru>
2016-06-20 14:21:05 +0300

    First Commit

---
+++ WebContent/project/main.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define(['sap/ui/core/mvc/Controller'],function(Controller){
+    
+    return Controller.extend('project.main',{
+       
+        onInit: function(){
+            var json = new sap.ui.model.json.JSONModel('./position.json');
+			this.getView().setModel(json);
+        },
+		
+		todoGroupFactory: function(oGroup){
+			return new sap.m.GroupHeaderListItem({
+				title: oGroup.key === '' ? '' : oGroup.key,
+				upperCase : false
+			})	
+			
+		}
+    })
+})
