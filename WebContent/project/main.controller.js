daaaa06eb02badfcb5cc09790013879b301c546f
Илья <i.ivannikov@molga.ru>
2016-06-21 15:19:35 +0300

    Add visible logic

--- WebContent/project/main.controller.js
+++ WebContent/project/main.controller.js
@@ -14,13 +14,17 @@ sap.ui.define(['sap/ui/core/mvc/Controller'],function(Controller){
 			})
 		},
 				
-		clickIcon : function(){
-			alert('something');
+		closeCommentItem : function(oEvent){
+			
+			var oContext = oEvent.getSource().getBindingContext();
+			var oModel = oContext.getModel();
+			oModel.setProperty(oContext.getPath()+'/visible',false);
+		
 		},
 		
 		popoverClick: function(oEvent){
 			
-			if(! this.oPopover){
+			if(!this.oPopover){
 				
 				this.oPopover = sap.ui.xmlfragment('fragments.Popover');
 				this.getView().addDependent(this.oPopover);
@@ -31,6 +35,10 @@ sap.ui.define(['sap/ui/core/mvc/Controller'],function(Controller){
 				this.oPopover.openBy(oButton);
 			})
 			
+		},
+		
+		dateSelect: function(){
+			alert('date selected');
 		}
 			
 		
